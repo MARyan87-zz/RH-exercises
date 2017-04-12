48fe684392300574367a24252f9c6d858e1d91b6
MickeyR <michael.anthony.ryan@ibm.com>
2017-04-11 20:23:07 -0400

    remove unnecessary consolelog

--- sampleproject/app/components/utils/helper.js
+++ sampleproject/app/components/utils/helper.js
@@ -3,8 +3,6 @@ var axios = require("axios");
 var helper = {
 
   getUsers: function (params) {
-  	console.log("running req");
-    
     return axios.get("/getUsers")
       .then(function (response) {
         return JSON.parse(response.data);
